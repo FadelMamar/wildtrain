b48c99d168df2bd4deda6f774c1fd0c3717dca8e
fadel <fadel>
2025-08-10 20:00:07 +0200

    change to posix path

--- scripts/launch_mlflow.bat
+++ scripts/launch_mlflow.bat
@@ -1,4 +1,4 @@
 call cd /d %~dp0 && cd ..
-call uv run --no-sync mlflow server --backend-store-uri runs\mlflow --host 0.0.0.0 --port 5000
+call uv run --no-sync mlflow server --backend-store-uri runs/mlflow --host 0.0.0.0 --port 5000
 
 
