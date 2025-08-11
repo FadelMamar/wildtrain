a8f431f34d51b3f90edd743d2d01889885a73963
fadel <fadel>
2025-08-11 12:23:18 +0200

    update

--- examples/loading_registered_model.py
+++ examples/loading_registered_model.py
@@ -1,6 +1,9 @@
 from wildtrain.utils.mlflow import load_registered_model
-
-model,metadata = load_registered_model(alias='demo',name='detector')
+import torch
+model,metadata = load_registered_model(alias='demo',name='detector',load_unwrapped=True)
 
 print(model)
 print(metadata)
+
+
+print(model.predict(torch.rand(1,3,640,640)))
--- scripts/register_model.bat
+++ scripts/register_model.bat
@@ -3,3 +3,6 @@ call cd /d "%~dp0" && cd ..
 call uv run --no-sync wildtrain register localizer --config configs/registration/localizer_registration_example.yaml
 
 call uv run --no-sync wildtrain register classifier --config configs/registration/classifier_registration_example.yaml
+
+call uv run --no-sync wildtrain register detector --config configs/registration/detector_registration_example.yaml
+
