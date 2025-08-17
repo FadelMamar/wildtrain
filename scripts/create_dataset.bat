fa4adf963cef0cac4328d6adbc5c262ace6c0606
fadel <fadel.seydou@gmail.com>
2025-08-18 01:49:50 +0200

    update

--- scripts/create_dataset.bat
+++ scripts/create_dataset.bat
@@ -1,3 +1,2 @@
 call cd /d "%~dp0" && cd ..
-call set CONFIG_FILE=configs\datapreparation\import-config-example.yaml
-call uv run wildata import-dataset --config %CONFIG_FILE%
+call uv run wildata import-dataset --config configs\datapreparation\savmap.yaml
