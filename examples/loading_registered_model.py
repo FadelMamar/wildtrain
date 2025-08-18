fd76399ed3f2cbad4d6ee8626ed2dc3c00d66354
fadel <fadel.seydou@gmail.com>
2025-08-18 11:15:12 +0200

    update

--- examples/loading_registered_model.py
+++ examples/loading_registered_model.py
@@ -1,9 +1,29 @@
 from wildtrain.utils.mlflow import load_registered_model
+from wildtrain.models.detector import Detector
 import torch
-model,metadata = load_registered_model(alias='demo',name='detector',load_unwrapped=True)
-
-print(model)
-print(metadata)
+from omegaconf import OmegaConf
 
 
-print(model.predict(torch.rand(1,3,640,640)))
+def load_detector():
+    model,metadata = load_registered_model(alias='demo',name='detector',load_unwrapped=True)
+
+    print(model.classifier)
+    print(metadata)
+
+    try:
+        print(model.predict(torch.rand(1,3,640,640)))
+    except Exception as e:
+        print(model(torch.rand(1,3,640,640)))
+
+def load_from_config():
+    cfg = OmegaConf.load("models-registry/detector/9/artifacts/detector_registration_example.yaml")
+    localizer_cfg = cfg.localizer.yolo
+    localizer_cfg.weights = "models-registry/detector/9/artifacts/best.pt"
+    model = Detector.from_config(localizer_config=localizer_cfg,
+                                        classifier_ckpt=r"models-registry\detector\9\artifacts\best.ckpt")
+    print(model.localizer is None,model.classifier is None)
+
+
+# load_from_config()
+
+load_detector()
