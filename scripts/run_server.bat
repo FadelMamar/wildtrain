9849f6e84a1824f3ffda8cae27331068065e4649
fadel <fadel.seydou@gmail.com>
2025-08-24 01:21:05 +0200

    update

--- scripts/run_server.bat
+++ scripts/run_server.bat
@@ -2,6 +2,6 @@ call cd /d "%~dp0" && cd ..
 
 call set MLFLOW_TRACKING_URI=http://127.0.0.1:5000
 
-:: call uv run wildtrain run-server --config configs/inference.yaml
+call uv run wildtrain run-server --config configs/inference.yaml
 
-call uv run mlflow models serve -m models:/detector/9 -p 4141 --no-conda --workers 1 --host 0.0.0.0
+:: call uv run mlflow models serve -m models:/detector/9 -p 4141 --no-conda --workers 1 --host 0.0.0.0
